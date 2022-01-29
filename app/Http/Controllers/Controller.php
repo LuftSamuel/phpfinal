37ede628456b4703e67629413ce0b7c4d4600b9b
unknown <70414072+LuftSamuel@users.noreply.github.com>
2022-01-29 10:02:48 -0300

    cabecera y pie individuales, vistas de administrador
    Ahora la cabecera y el pie son archivos individuales, elimine la vista acercade que no pensaba usar, añadi plantillas para la vista de administrador

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,8 +11,3 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
- 
-
-  //asd
-
-asd
---
+++ resources/views/cabecera.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{str_replace("_"," ",config('app.name'))}}</title>
+    <link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('assets/fontawesome/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/templatemo-style.css')}}">
+<!--
+    
+TemplateMo 556 Catalog-Z
+
+https://templatemo.com/tm-556-catalog-z
+
+-->
+</head>
+<body>
+    <!-- Page Loader -->
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+
+    </div>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">
+                <i class="fas fa-film mr-2"></i>
+                {{str_replace("_"," ",config('app.name'))}}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link nav-link-1" href="index.html">Articulos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-2" href="videos.html">Contacto</a>
+                </li>
+                @if(isset($_SESSION['nombre']))
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-4" href="contact.html">Panel de administrador</a>
+                    </li>
+                @endif
+            </ul>
+            </div>
+        </div>
+    </nav>
