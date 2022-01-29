87274b173af9be6c3809784237ac8f789f021193
unknown <70414072+LuftSamuel@users.noreply.github.com>
2022-01-29 18:56:13 -0300

    Nuevas vistas
    Vista para login del admin, dos vistas para cargar productos (2 tipos de productos).

---
+++ resources/views/crear_articulo_menor.blade.php
@@ -0,0 +1,37 @@
+@include('cabecera')
+
+<div style="margin: auto; max-width: 600px; width: 100%; padding: 2em;">
+    <form method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="inputText" class="form-label">Nombre:</label>
+            <input type="text" name="title" class="form-control" id="inputText" required>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+                Familia
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <li><button class="dropdown-item" type="button">familia 1</button></li>
+                <li><button class="dropdown-item" type="button">familia 2</button></li>
+                <li><button class="dropdown-item" type="button">familia 3</button></li>
+            </ul>
+        </div>
+        <div class="mb-3">
+            <label for="formFileMultiple" class="form-label">Seleccionar archivo/s</label>
+            <input class="form-control" type="file" name="file" id="formFileMultiple" required>
+        </div>   
+        <div class="mb-3">
+            <label for="inputText" class="form-label">Precio unitario:</label>
+            <input type="text" name="title" class="form-control" id="inputText" required>
+        </div>
+        <div class="mb-3">
+            <label for="inputText" class="form-label">Cantidad en stock:</label>
+            <input type="text" name="title" class="form-control" id="inputText" required>
+        </div>
+        <div class="col-12">
+            <button class="btn btn-primary" type="submit">Subir</button>
+        </div>
+    </form>
+</div>
+
+@include('pie')
